e42ae16bb032ff015e79cbdc2bd2c1ea8a6c55a6
Lutfi92 <60707776+Lutfi92@users.noreply.github.com>
2022-10-02 18:33:51 +0200

    lay-out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,24 @@
 import './Header.css'
 
 import logo from '../../assets/images/logo.png'
+
+import NavItem, {NavItemDropdown} from '../NavItem/NavItem'
 const Header = () => {
   return (
     <div className="navbar navbar-expand-md bg-dark navbar-dark ">
         <div className="container">
-            <a href="#" class="navbar-brand">
+            <a href="#top" className="navbar-brand">
               <img src={logo} alt = ""/>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu">
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="mainmenu">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item"><a href="#hero" className="nav-link">Home</a></li>
-                    <li className="nav-item"><a href="#features" class="nav-link">Browse</a></li>
+                    <li className="nav-item"><a href="#features" className="nav-link">Browse</a></li>
                     <li className="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Details</a>
+                        <a href="#top" calssName = "nav-link dropdown-toggle" data-bs-toggle="dropdown">Details</a>
                         <ul class="dropdown-menu">
                             <li><a href="#learn" calssName="dropdown-item">Fortnite</a></li>
                             <li><a href="#next" calssName="dropdown-item">Call of Duty</a></li>
